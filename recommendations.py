a4402e16584837818601b1d7cd7b73a5c8347721
Andrin Foster <OcastaEshu@Ocastas-MacBook-Pro.local>
2015-03-14 11:30:30 -0400

    more changes

--- recommendations.py
+++ recommendations.py
@@ -1,4 +1,3 @@
-import rethinkdb as r
 
 # A dictionary of movie critics and their ratings of a small
 # set of movies
@@ -20,54 +19,7 @@ critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
  'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
 'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
 
-# Gets recommendations for a person by using a weighted average
-# of every other user's rankings
-def getRecommendations(prefs,person,similarity=sim_pearson):
-	totals={}
-	simSums={}
-	for other in prefs:
-		# don't compare me to myself
-		if other==person: continue
-		sim=similarity(prefs,person,other)
 
-		# ignore scores of zero or lower
-		if sim<=0: continue
-		for item in prefs[other]:
-
-			# only score movies I haven't seen yet
-			if item not in prefs[person] or prefs[person][item]==0:
-				# Similarity * Score
-				totals.setdefault(item,0)
-				totals[item]+=prefs[other][item]*sim
-				# Sum of similarities
-				simSums.setdefault(item,0)
-				simSums[item]+=sim
-	# Create the normalized list
-	rankings=[(total/simSums[item],item) for item,total in totals.items( )]
-
-	# Return the sorted list
-	rankings.sort( )
-	rankings.reverse(
-	return rankings
-
-
-def transformPrefs(prefs): 
-	result={}
-	for person in prefs:
-		for item in prefs[person]:
-			result.setdefault(item,{})
-			# Flip item and person
-			result[item][person]=prefs[person][item]
-	return result
-
-# Returns the best matches for person from the prefs dictionary.
-# Number of results and similarity function are optional params.
-def topMatches(prefs,person,n=5,similarity=sim_pearson):
-	scores=[(similarity(prefs,person,other),other) for other in prefs if other!=person]
-	# Sort the list so the highest scores appear at the top
-	scores.sort( )
-	scores.reverse( )
-	return scores[0:n]
 
 
 # Returns the Pearson correlation coefficient for p1 and p2
@@ -102,6 +54,58 @@ def sim_pearson(prefs,p1,p2):
 	r=num/den
 
 	return r
+
+def transformPrefs(prefs): 
+	result={}
+	for person in prefs:
+		for item in prefs[person]:
+			result.setdefault(item,{})
+			# Flip item and person
+			result[item][person]=prefs[person][item]
+	return result
+
+# Returns the best matches for person from the prefs dictionary.
+# Number of results and similarity function are optional params.
+def topMatches(prefs,person,n=5,similarity=sim_pearson):
+	scores=[(similarity(prefs,person,other),other) for other in prefs if other!=person]
+	# Sort the list so the highest scores appear at the top
+	scores.sort( )
+	scores.reverse( )
+	return scores[0:n]
+
+
+# Gets recommendations for a person by using a weighted average
+# of every other user's rankings
+def getRecommendations(prefs,person,similarity=sim_pearson):
+	totals={}
+	simSums={}
+	for other in prefs:
+		# don't compare me to myself
+		if other==person: continue
+		sim=similarity(prefs,person,other)
+
+		# ignore scores of zero or lower
+		if sim<=0: continue
+		for item in prefs[other]:
+
+			# only score movies I haven't seen yet
+			if item not in prefs[person] or prefs[person][item]==0:
+				# Similarity * Score
+				totals.setdefault(item,0)
+				totals[item]+=prefs[other][item]*sim
+				# Sum of similarities
+				simSums.setdefault(item,0)
+				simSums[item]+=sim
+	# Create the normalized list
+	rankings=[(total/simSums[item],item) for item,total in totals.items( )]
+
+	# Return the sorted list
+	rankings.sort( )
+	rankings.reverse()
+	return rankings
+
+
+
 
 
 
