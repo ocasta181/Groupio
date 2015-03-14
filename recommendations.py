0dc272c7f0b03211bce77f59c8011e2570f3f0c6
Andrin Foster <OcastaEshu@Ocastas-MacBook-Pro.local>
2015-03-14 13:10:39 -0400

    Using item based also

--- recommendations.py
+++ recommendations.py
@@ -14,7 +14,7 @@
 
 
 
-import math
+from math import sqrt
 
 
 # A dictionary of movie critics and their ratings of a small
@@ -39,6 +39,21 @@ critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
 
 group1 = {'Lisa Rose', 'Mick LaSalle', 'Toby'}
 
+# Returns a distance-based similarity score for person1 and person2
+def sim_distance(prefs,person1,person2):
+	# Get the list of shared_items
+	si={}
+	for item in prefs[person1]:
+		if item in prefs[person2]:
+			si[item]=1
+
+	# if they have no ratings in common, return 0
+	if len(si)==0: return 0
+
+	# Add up the squares of all the differences
+	sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
+
+	return 1/(1+sum_of_squares)
 
 # Returns the Pearson correlation coefficient for p1 and p2
 def sim_pearson(prefs,p1,p2):
@@ -66,7 +81,7 @@ def sim_pearson(prefs,p1,p2):
 
 	# Calculate Pearson score
 	num=pSum-(sum1*sum2/n)
-	den=math.sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+	den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
 	if den==0: return 0
 
 	r=num/den
@@ -184,8 +199,72 @@ def getHighestRecommendation(recommendations):
 	return index
 
 
-rec = getGroupRecommendations(group1)
-output = getHighestRecommendation(rec)
+def getRecommendedItems(prefs,itemMatch,user):
+	userRatings=prefs[user]
+	scores={}
+	totalSim={}
+	# Loop over items rated by this user
+	for (item,rating) in userRatings.items( ):
+		# Loop over items similar to this one
+		for (similarity,item2) in itemMatch[item]:
+			# Ignore if this user has already rated this item
+			if item2 in userRatings: continue
+
+			# Weighted sum of rating times similarity
+			scores.setdefault(item2,0)
+			scores[item2]+=similarity*rating
+
+			# Sum of all the similarities
+			totalSim.setdefault(item2,0)
+			totalSim[item2]+=similarity
+
+	# Divide each total score by total weighting to get an average
+	rankings=[(score/totalSim[item],item) for item,score in scores.items( )]
+
+	# Return the rankings from highest to lowest
+	rankings.sort( )
+	rankings.reverse( )
+
+	return rankings
+
+
+
+def calculateSimilarItems(prefs,n=10):
+	# Create a dictionary of items showing which other items they
+	# are most similar to.
+	result={}
+	# Invert the preference matrix to be item-centric
+	itemPrefs=transformPrefs(prefs)
+	c=0
+	for item in itemPrefs:
+		# Status updates for large datasets
+		c+=1
+		if c%100==0: print "%d / %d" % (c,len(itemPrefs))
+		# Find the most similar items to this one
+		scores=topMatches(itemPrefs,item,n=n,similarity=sim_distance)
+		result[item]=scores
+
+	return result
+
+def groupSimilarItems(group):
+	weightedRec={}
+	for person in group:
+		for score, movieName in getRecommendedItems(critics,itemsim,person):
+			if movieName in weightedRec.keys():
+				weightedRec[movieName] += score 
+			else:
+				weightedRec[movieName] = score
+	
+	return weightedRec
+
+itemsim = calculateSimilarItems(critics)
+
+output = groupSimilarItems(group1)
+
+
+
+#rec = getGroupRecommendations(group1)
+#output = getHighestRecommendation(rec)
 print output
 
 
